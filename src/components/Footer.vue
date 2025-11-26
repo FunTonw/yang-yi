9c62c3e437530aefbb994c10ac40ddd11ebb38c6
adam <kd20220905>
2025-11-26 23:13:12 +0800

    fix: footer 使用

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,8 @@
   <div class="max-w-7xl mx-auto px-4 py-12">
     <div class="text-center text-gray-400 text-xs mt-12">
       Copyright © 2025
-      <span class="font-bold text-gray-700">Wong & Ouyang (HK) LTD</span> All
-      rights reserved. Powered by
-      <span class="text-gray-600">iGears Technology Limited (iGears)</span>.
+      <span class="font-bold text-gray-700">yang-yi</span> All rights reserved.
+      Powered by <span class="text-gray-600">TSUNG YI</span>. v0.0.1
     </div>
   </div>
 </template>
